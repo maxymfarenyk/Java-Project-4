c88fcdcc64eb8fc70e6b232826b3662ce16f59a8
Farenyk Maximko <maximfarenik@gmail.com>
2020-07-27 19:41:30 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        float first, second, result;
+        System.out.print("Enter first num: ");
+        first = num.nextFloat();
+        System.out.print("Enter second num: ");
+        second = num.nextFloat();
+        result = first + second;
+        result += 2;
+        System.out.println("Result is - " + result);
+    }
+}
